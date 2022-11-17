d96c61352ee13b61ee10178525f15b3b985ef91f
ConorCollier1 <98527408+ConorCollier1@users.noreply.github.com>
2022-11-17 00:53:23 +0000

    Add project files.

---
+++ Assessment2/PlayingCard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment2
+{
+    internal class PlayingCard
+    {
+        private string Value;
+        private string Suit;
+
+        public PlayingCard(string value, string suit)
+        {
+            Value = value;
+            Suit = suit;
+        }
+
+        public override string ToString()
+        {
+            return $"{Value} of {Suit}";
+        }
+    }
+}
