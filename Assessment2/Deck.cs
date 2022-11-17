f5c21664d8af98d96cc43e21e7cb5359817df070
ConorCollier1 <98527408+ConorCollier1@users.noreply.github.com>
2022-11-17 01:21:53 +0000

    program to create cards, create a deck and shuffle and output shows shuffled deck

--- Assessment2/Deck.cs
+++ Assessment2/Deck.cs
@@ -8,6 +8,47 @@ namespace Assessment2
 {
     internal class Deck
     {
-        
+        private PlayingCard[] deck;
+        private int card;
+        private const int deckSize = 52;
+        private Random randomNumber=new Random();
+
+        public Deck()
+        {
+            //create card arrays
+            string[] values = {"Ace","Two","Three","Four","Five","Six",
+                "Seven","Eight","Nine","Ten","Jack","Queen","King"};
+            string[] suits = { "Clubs", "Diamonds", "Hearts", "Spades" };
+
+            deck = new PlayingCard[deckSize];
+            for (int i = 0; i < deck.Length; i++)//loop adding each card to deck one by one
+            {
+                deck[i] = new PlayingCard(values[i % 11], suits[i / 13]);
+            }
+        }
+
+        public void ShuffleDeck()
+        {
+            card = 0;   //set current card to zero
+            for(int x=0; x<deck.Length; x++)//loop through all cards in deck and shuffle them
+            {
+                int y = randomNumber.Next(deckSize);
+                PlayingCard temp=deck[x];
+                deck[x] = deck[y];
+                deck[y] = temp;
+            }
+        }
+
+        public PlayingCard DealCard()
+        {
+            if (card < deck.Length)     //check its in bounds of array
+            {
+                return deck[card++];
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 }
--- Assessment2/Program.cs
+++ Assessment2/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Deck deck1=new Deck();
+            deck1.ShuffleDeck();
+            for (int i = 0; i < 52; i++)
+            {
+                Console.Write("{0,-19}", deck1.DealCard());
+                if((i+1) % 4 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
