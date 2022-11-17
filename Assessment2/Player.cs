41e7a5392ec8434e666797c455bfd3059aec15e9
ConorCollier1 <98527408+ConorCollier1@users.noreply.github.com>
2022-11-17 11:18:17 +0000

    added player class

---
+++ Assessment2/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment2
+{
+    internal class Player
+    {
+        public PlayingCard[] hand;
+        public int points;
+        public int numCardsInHand;
+        public string name;
+
+        public Player()
+        {
+            hand = new PlayingCard[10];
+            numCardsInHand = 0;
+            points = 0;
+        }
+    }
+}
