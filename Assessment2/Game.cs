cb01d9d736b4c9d3e8e4da0f265d09a329040874
ConorCollier1 <98527408+ConorCollier1@users.noreply.github.com>
2022-11-18 00:00:21 +0000

    Added a method to handle aces equalling 1 or 11

--- Assessment2/Game.cs
+++ Assessment2/Game.cs
@@ -27,12 +27,29 @@ namespace Assessment2
                 //player turn
                 Console.WriteLine("*** Player's Turn ***");
                 // Draw the first two cards for the Player
-                PlayingCard card1 = deck1.DealCard(ref player1);
-                PlayingCard card2 = deck1.DealCard(ref player1);
-                Console.WriteLine($"Card Dealt is {card1}");              
-                Console.WriteLine($"Card Dealt is {card2}");
+                Console.WriteLine($"Card Dealt is {deck1.DealCard(ref player1)}");              
+                Console.WriteLine($"Card Dealt is {deck1.DealCard(ref player1)}");
                 OutputPoints(player1);
-            } while(playAgain=="Y");
+                CheckPoints(player1);
+                bool underTwentyOne = true;
+                string stick_twist = "";
+
+                do
+                {
+                    Console.Write("Do you want to stick or twist - (s/t)?");
+                    stick_twist = Console.ReadLine().ToLower();
+
+                    if (stick_twist == "t")//if user selects twist -- give them another card
+                    {
+                        Console.WriteLine($"Card Dealt is {deck1.DealCard(ref player1)}");
+                        CheckForAce(player1);
+                        underTwentyOne = CheckPoints(player1);
+                    }
+                } while (underTwentyOne == true && stick_twist != "s");
+                Console.Write("Do you want to play again? y/n ");
+                playAgain = Console.ReadLine().ToLower();
+                Console.WriteLine("\n");
+            } while(playAgain=="y");
         }
 
         static public bool CheckPoints(Player player)
@@ -49,6 +66,22 @@ namespace Assessment2
         static public void OutputPoints(Player player)            //outputs players current cards and point total
         {
             Console.WriteLine($"Current points: {player.points}");
+        }
+        static public void CheckForAce(Player player) //check for aces 
+        {
+            bool aceToOnePoint = false;
+            if(player.points>21) //check if player can be saved by an ace if they go over 21
+            {
+                for(int i = 0; i < player.numCardsInHand; i++)//loop through cards in players hand
+                {
+                    if (player.hand[i].Points==11&&aceToOnePoint==false)//if points equal 11 then card is ace
+                    {
+                        player.hand[i].Points = 1;          //change ace points from 11 to 1
+                        player.points -= 10;    //take 10 points from total
+                        aceToOnePoint = true;
+                    }
+                }
+            }
         }
     }
 }
